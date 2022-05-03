a772c5651bbdfd76f102a0bfc9d4e3c2704dd9e7
Miss Karla Ileana Vega Rios <89174547+karlaavegga@users.noreply.github.com>
2022-05-03 16:43:16 -0600

    Add files via upload

---
+++ piezas.js
@@ -0,0 +1,14 @@
+function piezaT(){
+    a = createSprite(200,200,40,40);
+    b = createSprite(240,200,40,40);
+    c = createSprite(160,200,40,40);
+    d = createSprite(200,240,40,40);
+    a.shapeColor = "blue"
+    b.shapeColor = "blue"
+    c.shapeColor = "blue"
+    d.shapeColor = "blue"
+}
+
+function piezaLizq(){
+    
+}
---
+++ sketch.js
@@ -0,0 +1,14 @@
+function preoload(){
+
+}
+
+function setup(){
+  createCanvas(1000,550);
+  piezaT();
+
+}
+
+function draw(){
+  background(180);
+  drawSprites();
+}
